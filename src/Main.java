3dfaef2da8295362dbb9ed00bc69f90a819f92d4
ng5109-cloud <ng5109@srmist.edu.in>
2026-04-23 19:17:35 +0530

    Initial commit - UC1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/**
+ * UseCase1HotelBookingApp
+ * 
+ * This class represents the entry point of the Hotel Booking Application.
+ * It demonstrates how a Java program starts execution and prints output.
+ * 
+ * @author ng5109
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Main method - entry point of the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=================================");
+
+    }
+}
